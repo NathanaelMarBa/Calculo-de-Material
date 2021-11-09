f502d54534936c5e55246bec03f68943eff05f6b
NatanaelMarBa <kevin_1321124060@uptecamac.edu.mx>
2021-11-09 10:09:40 -0600

    ACT

--- Calculo.js
+++ Calculo.js
@@ -29,11 +29,33 @@ function pared(){
     let longitud = parseFloat(prompt("Ingrese la longitud de la pared"));
     let altura = parseFloat(prompt("Ingrese el largo de la pared"));
     let liga = 0.01;
+    let ancho = parseFloat(prompt("Ingrese el ancho del bloc a utilizar \n 1.10cm \n 2.15cm \n 3. 20cm \n 4. 25cm"));
+    let bloc1 = 10;
+    let bloc2 = 15;
+    let bloc3 = 20;
+    let bloc4 = 25;
+
+    if(ancho == 1){
+        ancho = bloc1;
+    } else if(ancho == 2){
+        ancho = bloc2;
+    } else if(ancho == 3){
+        ancho = bloc3;
+    } else if(ancho == 4){
+        ancho = bloc4;
+    }
+
     let longitudbloc = 0.40;
     let alturabloc = 0.20;
 
     bloquesancho = (longitud) / (longitudbloc + liga);
     bloquesalto = (altura) / (alturabloc + liga);
     let total = bloquesancho * bloquesalto;
-    alert("El numero de bloques a utilizar es: " + Math.round(total));
+    let desperdicio = total * 0.5;
+    total = total + desperdicio;
+
+    let mortero = (longitudbloc + alturabloc)*(ancho)*(liga);
+    let totalmortero = Math.round(total) * mortero;
+    alert("El numero de blocs a utilizar es: " + Math.round(total));
+    alert("El total de mortero a utilizar es: " + totalmortero.toFixed(4));
 }
